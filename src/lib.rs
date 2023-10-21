0c7d5b42c657838cae3143406f001510d5f3e78e
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-21 10:14:54 +0530

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,22 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+
+pub fn search<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "feat";
+        let content = "\
+Rust is a powerful language.
+It is most loved language.
+It has most uniqe features.";
+        assert_eq!(vec!["It has most uniqe features."], search(query, content));
+    }
+}
