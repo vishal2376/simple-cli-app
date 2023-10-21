79eee2664663f798575f5466685f50dc05938739
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-21 09:21:38 +0530

    specify args var

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args)
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Query : {}", query);
+    println!("Filename : {}", filename);
 }
