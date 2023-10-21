33f3fdb63f509d74ae6dbcf6ba277ee040b9de46
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-21 09:40:52 +0530

    refactor: config struct and impl block

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,25 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args);
 
-    println!("Query : {}", query);
-    println!("Filename : {}", filename);
+    println!("Query : {}", config.query);
+    println!("Filename : {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Failed to read file.");
+    let contents = fs::read_to_string(config.filename).expect("Failed to read file.");
 
     println!("\nFile Content:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    (query, filename)
+        Config { query, filename }
+    }
 }
