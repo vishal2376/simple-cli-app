95b71395e6e67093c1e17c51a20f0f9d39950700
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-21 09:33:28 +0530

    refactor: func for parse args

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Query : {}", query);
     println!("Filename : {}", filename);
@@ -12,4 +11,11 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Failed to read file.");
 
     println!("\nFile Content:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
