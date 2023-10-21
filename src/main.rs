2c3b0481175fb4bb90f22746a1be3057c415d74f
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-21 09:48:16 +0530

    refactor: handle args error msg

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem Parsing Arguments : {}", err);
+        process::exit(1)
+    });
 
     println!("Query : {}", config.query);
     println!("Filename : {}", config.filename);
@@ -18,10 +21,13 @@ struct Config {
     filename: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not Enought Arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
